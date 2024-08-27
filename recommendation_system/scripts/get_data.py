38a00e968f5ae064423872f8c325f8595b2a9a6a
Vigrel <eller.vini@gmail.com>
2024-08-27 17:04:04 -0300

    refactor: modularize and enhance text processing
    - Add `TextProcessor` class. Handle translation, lemmatization
    - Enhanced error handling

--- recommendation_system/scripts/get_data.py
+++ recommendation_system/scripts/get_data.py
@@ -1,28 +1,98 @@
 import pickle
+import time
 
+import nltk
 import pandas as pd
 from cocktail_scraper import CocktailScraper
 from crawl import Crawler
+from googletrans import Translator
+from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-if __name__ == "__main__":
-    base_url = ""
-    start_url = ""
+
+class TextProcessor:
+    def __init__(self):
+        nltk.download("wordnet")
+        nltk.download("omw-1.4")
+        self.lemmatizer = WordNetLemmatizer()
+        self.translator = Translator()
+
+    def translate_text(self, text):
+        try:
+            return self.translator.translate(text, src="pt", dest="en").text
+        except Exception as e:
+            return text
+
+    def translate_batch(self, df, batch_size=10, sleep_time=1):
+        translated_texts = []
+        for start in range(0, len(df), batch_size):
+            batch = df.iloc[start : start + batch_size]
+            translated_batch = batch["how_to"].apply(self.translate_text)
+            translated_texts.extend(translated_batch)
+            time.sleep(sleep_time)
+            # TODO: add logging
+        return translated_texts
+
+    def lemmatize_text(self, text):
+        return self.lemmatizer.lemmatize(text)
+
+    def process_data(self, data_path):
+        try:
+            df = pd.read_csv(f"{data_path}/cocktail_data.csv").astype(str)
+        except Exception as e:
+            return None
+
+        df["how_to_translated"] = self.translate_batch(df)
+
+        df["whole_text"] = df.apply(
+            lambda row: f"{row.garnish} {row.how_to_translated} {row.comment} {row.history}", axis=1
+        )
+        df["whole_text"] = df["whole_text"].apply(self.lemmatize_text)
+
+        try:
+            df.to_csv(f"{data_path}/cocktail_data_silver.csv", index=False)
+        except Exception as e:
+            ...
+
+        return df["whole_text"]
+
+
+class ModelTrainer:
+    def __init__(self, vectorizer=None):
+        self.vectorizer = vectorizer or TfidfVectorizer(
+            strip_accents="unicode", stop_words="english", lowercase=True
+        )
+
+    def train_model(self, model_path, text):
+        tfidf_matrix = self.vectorizer.fit_transform(text)
+
+        try:
+            with open(f"{model_path}/vectorizer.pk", "wb") as f:
+                pickle.dump(self.vectorizer, f)
+            with open(f"{model_path}/tfidf_matrix.pk", "wb") as f:
+                pickle.dump(tfidf_matrix, f)
+        except Exception as e:
+            ...
+
+        return self.vectorizer, tfidf_matrix
+
+
+def main(base_url, start_url):
     scraper = CocktailScraper()
     crawler = Crawler(base_url, start_url, crawl_limit=10)
-    crawler.crawl(scraper)
 
-    df = pd.read_csv(f"{crawler.data_path}/cocktail_data.csv")
-    df = df.astype(str)
-    df["whole_text"] = df.garnish + " " + df.comment + " " + df.history
+    try:
+        crawler.crawl(scraper)
+    except Exception as e:
+        return
 
-    vectorizer = TfidfVectorizer(
-        strip_accents="unicode", stop_words="english", lowercase=True
-    )
-    tfidf_matrix = vectorizer.fit_transform(df.whole_text)
+    text_processor = TextProcessor()
+    text = text_processor.process_data(crawler.data_path)
+    if text is None:
+        return
 
-    with open(f"{crawler.model_path}/vectorizer.pk", "wb") as f:
-        pickle.dump(vectorizer, f)
+    model_trainer = ModelTrainer()
+    model_trainer.train_model(crawler.model_path, text)
 
-    with open(f"{crawler.model_path}/tfidf_matrix.pk", "wb") as f:
-        pickle.dump(tfidf_matrix, f)
+if __name__ == "__main__":
+    main()
